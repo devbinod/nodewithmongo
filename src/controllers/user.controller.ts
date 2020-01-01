fc86fe686dc0b49dd1ef678dda7c7eb1e8c38c50
binod <bpant@deerhold.org>
2020-01-01 15:10:08 +0545

    status code added.

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -2,8 +2,7 @@ import { Request, Response } from "express";
 import { UserService } from "../services/user.service";
 import { IModel } from "../models/model";
 import e = require("express");
-import { IUser } from "../interfaces/user";
-
+import {SUCCESS,DELETED,UPDATED,CREATED, INTERNAL_SERVER_ERROR} from '../statuscode/statuscode'
 export class UserController {
   private model: IModel;
   private userService: UserService;
@@ -15,7 +14,7 @@ export class UserController {
 
   findAll = async (req: Request, res: Response) => {
     let userList = await this.userService.findAll();
-    res.status(200);
+    res.status(SUCCESS);
     res.json({
       data: userList
     });
@@ -23,7 +22,7 @@ export class UserController {
 
   findById = async (req: Request, res: Response) => {
     let user = await this.userService.findById(req.param("id"));
-    res.status(200);
+    res.status(SUCCESS);
     res.json({
       data: user
     });
@@ -31,21 +30,19 @@ export class UserController {
 
   save = async (req: Request, res: Response) => {
     const resp = await this.userService.add(req.body);
-    res.status(201);
+    res.status(CREATED);
     res.send(resp);
   };
 
   update = async (req: Request, res: Response) => {
     let user = await this.userService.update(req.param("id"), req.body);
-    console.log(`User ${user}`);
-    res.send(user);
+    res.status(UPDATED).send(user);
   };
 
   delete = async (req: Request, res: Response) => {
     let isDeleted = await this.userService.delete(req.param("id"));
-    console.log("dsfsd.........", isDeleted);
     if (isDeleted) {
-      res.status(204).send({
+      res.status(DELETED).send({
         data: [
           {
             message: "Successfully deleted"
@@ -53,7 +50,7 @@ export class UserController {
         ]
       });
     } else {
-      res.status(500).json({
+      res.status(INTERNAL_SERVER_ERROR).json({
         data: [
           {
             message: "Internal server error"
---
+++ src/statuscode/statuscode.ts
@@ -0,0 +1,5 @@
+export const SUCCESS = 200;
+export const CREATED = 201;
+export const UPDATED = 204;
+export const DELETED = 204;
+export const INTERNAL_SERVER_ERROR = 500;
