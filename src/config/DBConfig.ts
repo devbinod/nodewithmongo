258cb5e3f68612cbad9026f5a13fa3b052f30d9e
binod <bpant@deerhold.org>
2020-01-10 14:33:18 +0545

    timestamp added.

--- src/config/DBConfig.ts
+++ src/config/DBConfig.ts
@@ -1,18 +1,15 @@
-const MONGODB_CONNECTION: string = "mongodb://localhost:27017/hahaha";
+const MONGODB_CONNECTION: string = "mongodb://localhost:27017/socialmedia";
 import mongoose from "mongoose";
 export class DBConfig {
   constructor() {
     this.openConnection();
   }
 
-  
   openConnection = async () => {
-    await mongoose
-      .connect(MONGODB_CONNECTION)
-      .catch(err => {
-        console.log(`Oops something went wrong ${err}`);
-      });
+    await mongoose.connect(MONGODB_CONNECTION).catch(err => {
+      console.log(`Oops something went wrong ${err}`);
+    });
 
-      console.log(`Successfully`)
+    console.log(`Successfully`);
   };
 }
--- src/schemas/comment.schemas.ts
+++ src/schemas/comment.schemas.ts
@@ -3,11 +3,14 @@ import { USER, POST, COMMENT } from "./schemaConst";
 import { UserModel } from "../models/user";
 import { CommentModel } from "../models/comment";
 
-const schema: Schema = new Schema({
-  userId: { type: Schema.Types.ObjectId, ref: `${USER}` },
-  postId: { type: Schema.Types.ObjectId, ref: `${POST}` },
-  comment: String,
-  isLike: Boolean
-});
+const schema: Schema = new Schema(
+  {
+    userId: { type: Schema.Types.ObjectId, ref: `${USER}` },
+    postId: { type: Schema.Types.ObjectId, ref: `${POST}` },
+    comment: String,
+    isLike: Boolean
+  },
+  { timestamps: true }
+);
 
 export const commentSchema = mongoose.model<CommentModel>(`${COMMENT}`, schema);
